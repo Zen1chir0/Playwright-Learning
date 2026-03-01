e5fb6809bd3bc39617a6645cdfee50b1dbefd972
Kenneth Flororita <kennethflororita@gmail.com>
2026-03-01 15:50:54 +0800

    added login verifier and usage of beforeEach & test describe

--- tests/swagLabsLoginTest.spec.js
+++ tests/swagLabsLoginTest.spec.js
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-  test.beforeEach('landing page', async ({ page }) => {
+//test.describe to group up tests alike
+test.describe('Login Test', () => {
+  //simple usage of beforeEach function
+  test.beforeEach(async ({ page }) => {
      await page.goto('https://www.saucedemo.com/');
     
      await expect(page.locator('.login_logo')).toHaveText(/Swag labs/i);
@@ -11,3 +14,13 @@ import { test, expect } from '@playwright/test';
 
      await page.getByRole('button', { name: 'Login' }).click();
 })
+
+  test('login-verifier', async ({ page }) => {
+    //page goto isnt needed because the beforeEach brough me there
+
+
+    //verifies login
+    await expect(page.locator('.inventory_container')).toHaveText(/Sauce Labs Backpack/i);
+  });
+
+});
