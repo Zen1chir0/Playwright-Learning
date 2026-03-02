b0955cf06a4a680430909f185d8e8c755dae6c67
Kenneth Flororita <kennethflororita@gmail.com>
2026-03-02 15:28:12 +0800

    Completed(Spaghetti Coded, Just plain eye pain)

--- tests/e2eSwagLabs.spec.js
+++ tests/e2eSwagLabs.spec.js
@@ -13,8 +13,29 @@ test.describe('End to end test', () => {
         await page.getByRole('button', { name: 'Login' }).click();
     })
 
-    test('login-verifier', async ({ page }) => {
+    test('Dashboard -> Completion', async ({ page }) => {
         await expect(page.locator('.inventory_container')).toHaveText(/Sauce Labs Backpack/i);
 
+        await page.locator('#add-to-cart-sauce-labs-backpack').click();
+
+        //checkout
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
+
+        //another inputs for details
+        await page.getByPlaceholder('First Name').fill('Name');
+        await page.getByPlaceholder('Last Name').fill('Name');
+        await page.getByPlaceholder('Zip/Postal Code').fill('1122');
+    
+        await page.locator('[data-test="continue"]').click();
+
+        await page.locator('[data-test="finish"]').click();
+        await page.locator('[data-test="back-to-products"]').click();
+
+        await page.locator('#react-burger-menu-btn').click();
+        await page.locator('[data-test="logout-sidebar-link"]').click();
     });
 })
+
+//Completed it but I feel un impressed, all i did was direct things. never really coded it in a way thats innovative. this is just cluttered.
+//I discovered POM, a structured and cleaner way of implementating testing. I'll try to refactor this in POM.
