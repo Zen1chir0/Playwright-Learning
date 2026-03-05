3ff21b5f0252d25f529e057929a1b72c548bc603
Kenneth Flororita <kennethflororita@gmail.com>
2026-03-05 15:47:32 +0800

    Correction of my yesterday's push

--- tests/smoke/e2ePOM.spec.js
+++ tests/smoke/e2ePOM.spec.js
@@ -64,22 +64,3 @@ test.describe('should the login page allow login', () => {
 
     });
 });
-
-class CheckoutStep1 {
-    constructor(page) {
-        this.page = page;
-        this.firstName = page.locator('[data-test="firstName"]');
-        this.lastName = page.locator('[data-test="lastName"]');
-        this.postalCode = page.locator('[data-test="postalCode"]');
-        this.continue = page.locator('[data-test="continue"]');
-    }
-
-    async checkOut (firstName, lastName, postalCode) {
-        await this.firstName.fill(firstName);
-        await this.lastName.fill(lastName);
-        await this.postalCode.fill(postalCode);
-        await this.continue.click();
-    }
-}
-
-export default CheckoutStep1;
